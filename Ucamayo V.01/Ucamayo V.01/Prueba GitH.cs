4f4a0ed7b48c818de8b6d69c7aa2cc256196ebdd
victor braulio diaz ibarra <brauli-1998@live.com.mx>
2019-11-06 19:44:09 -0700

    Test Darks

---
+++ Ucamayo V.01/Ucamayo V.01/Prueba GitH.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ucamayo_V._01
+{
+    interface Prueba_GitH
+    {
+    }
+}
