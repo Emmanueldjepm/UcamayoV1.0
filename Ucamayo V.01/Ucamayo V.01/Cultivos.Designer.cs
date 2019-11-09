eb45a6ffa4b7775b92bd541dc45c4d5146b3c4de
Emmanueldjepm <57423219+Emmanueldjepm@users.noreply.github.com>
2019-11-09 15:09:00 -0700

    test

--- Ucamayo V.01/Ucamayo V.01/Cultivos.Designer.cs
+++ Ucamayo V.01/Ucamayo V.01/Cultivos.Designer.cs
@@ -87,7 +87,7 @@
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(106, 50);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(336, 195);
+            this.dataGridView1.Size = new System.Drawing.Size(346, 203);
             this.dataGridView1.TabIndex = 12;
             // 
             // Cultivos
