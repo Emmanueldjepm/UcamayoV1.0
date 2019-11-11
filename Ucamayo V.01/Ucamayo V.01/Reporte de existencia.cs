403e1d1e11556e11bd391e67bd373e291241b83a
emmanueldjpm <emmanuel1914@live.com.mx>
2019-11-10 21:04:39 -0600

    Revert "ghhj"
    This reverts commit 2c773674866b89e7358964351461d414e815bff0.

--- Ucamayo V.01/Ucamayo V.01/Reporte de existencia.cs
+++ Ucamayo V.01/Ucamayo V.01/Reporte de existencia.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Ucamayo_V._01
 {
-    public partial class Captura_de_ciclos_agricola : Form
+    public partial class Reporte_de_existencia : Form
     {
-        public Captura_de_ciclos_agricola()
+        public Reporte_de_existencia()
         {
             InitializeComponent();
         }
